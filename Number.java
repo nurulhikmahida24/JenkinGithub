a4e478defa39e28d3324d88454cb9b033e506686
nurulhikmahida24 <nurulhikmahidayat@gmail.com>
2020-01-07 04:52:08 +0700

    Menampilkan Bilangan Prima

--- Number.java
+++ Number.java
@@ -1,8 +1,27 @@
-public class Number {
-    public static void main(String args[]) {
-      System.out.println("Hello World");
-        for (int i=0; i<10; i++){
-            System.out.println(i);
-        }
-    }
-}
+public class Number
+{
+ public static void main (String[] args)
+ {
+ System.out.println("Inilah Bilangan Prima antara 1-100 = ");
+ int bil,bag;
+ for(bil=1;bil<100;bil++)
+ {
+  int i=0;
+  for(bag=1;bag<100;bag++)
+  {
+   if(bil%bag==0)
+   {
+    i++;
+   }
+
+  }
+   if ((i==2)&&(bil!=1))
+   {
+      System.out.println(bil);
+   }
+
+ }
+
+ }
+
+ }
